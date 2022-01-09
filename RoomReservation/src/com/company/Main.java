88d9b5eb29aa37e08a496ed9a98f7ad1d871db28
Rafael Silva <66369831+ralfsniper0102@users.noreply.github.com>
2022-01-09 12:55:05 -0300

    Completed

--- RoomReservation/src/com/company/Main.java
+++ RoomReservation/src/com/company/Main.java
@@ -9,15 +9,48 @@ public class Main {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
 
-
-
         System.out.print("Quantos quartos deseja reservar?: ");
         int quantity = sc.nextInt();
+        clearBuffer(sc);
 
+        Reservation[] reservations = new Reservation[11];
 
+        for(int i = 0; i < quantity; i++){
+            System.out.println("Reserva #" + (i+1));
+            System.out.print("Nome: ");
 
+            String nome = sc.nextLine();
+            System.out.print("Email: ");
+            String email = sc.next();
+            System.out.print("Quarto: ");
+            int room = sc.nextInt();
+            clearBuffer(sc);
+
+            reservations[room] = new Reservation(nome, email, room);
+
+        }
+
+        System.out.println("Quartos reservados: ");
+
+        for (Reservation obj : reservations) {
+            if(obj != null){
+                System.out.println(obj.Room
+                        + ": "
+                        + obj.Name
+                        + ", "
+                        + obj.Email
+                        );
+            }
+        }
+        System.out.println();
         sc.close();
 
 
+    }
+
+    private static void clearBuffer(Scanner scanner) {
+        if (scanner.hasNextLine()) {
+            scanner.nextLine();
+        }
     }
 }
--- RoomReservation/src/com/company/Reservation.java
+++ RoomReservation/src/com/company/Reservation.java
@@ -1,7 +1,15 @@
 package com.company;
 
 public class Reservation {
+
     public String Name;
     public String Email;
     public int Room;
+
+    public Reservation(String name, String email, int room) {
+        Name = name;
+        Email = email;
+        Room = room;
+    }
+
 }
